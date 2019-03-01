3330b8f88465a5afc643ec490ac4b4cb7b8988aa
Earving Morales <earving_08@hotmail.hotmail.com>
2019-03-01 16:46:32 -0600

    implemented filling of multiplication table

--- Lab4/Exercise1/multiplicationtable.php
+++ Lab4/Exercise1/multiplicationtable.php
@@ -1,11 +1,18 @@
  <?php
- echo "<table><tr>";
+ echo "<table><tr><td></td>";
+ for ($i=1; $i <= 100; $i++) {
+   echo "<td>$i</td>";
+ }
 
-for ($i=0; $i < 100; $i++) {
-  echo "<td>" + $i + "</td>";
-
+for ($i=1; $i <= 100; $i++) {
+  echo "<tr><td>$i</td>";
+  for ($j=1; $j <= 100; $j++) {
+    $product = $i * $j;
+    echo "<td> $product </td>";
+  }
+  echo "</tr>";
 }
 
-echo "</tr></table>";
+echo "</table>";
 
  ?>
