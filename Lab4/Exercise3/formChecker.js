9f1dfe838193ae503b7185e0a7010aa351e1b388
Earving <earving_08@hotmail.com>
2019-03-08 01:42:36 -0600

    added changing border to red if input is invalid

--- Lab4/Exercise3/formChecker.js
+++ Lab4/Exercise3/formChecker.js
@@ -6,6 +6,13 @@ function validate() {
     let tamt = document.getElementById("tamt");
     let famt = document.getElementById("famt");
     let damt = document.getElementById("damt");
+    
+    clearBorder(user);
+    clearBorder(pass);
+    clearBorder(tamt);
+    clearBorder(famt);
+    clearBorder(damt);   
+    
     let userCheck = userValidation(user);
     let passCheck = passValidation(pass);
     let tamtCheck = amtValidation(tamt);
@@ -16,6 +23,7 @@ function validate() {
 
 function passValidation(pass){
     if(pass.value == ""){
+        invalidField(pass);
         return false;
     }
     return true;
@@ -23,15 +31,31 @@ function passValidation(pass){
 
 function userValidation(user){
     if(user.value == ""){
+        invalidField(user);
         return false;
     }else if(!(/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(user.value))){
-        console.log("Invalid");
+        invalidField(user);
         return false;
     }
-    console.log("Valid");
     return true;
 }
 
 function amtValidation(amt){
-    return !(isNaN(amt.value) || amt.value < 0);
+    if(isNaN(amt.value) || amt.value < 0){
+       invalidField(amt);
+        return false;
+    }
+    return true;
 }
+
+
+function clearBorder(formBox){
+    formBox.style.border = "none";
+}
+
+
+
+function invalidField(field){
+    field.style.border = "red 1px solid";
+}
+
